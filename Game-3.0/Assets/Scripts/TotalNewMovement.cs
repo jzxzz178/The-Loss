6eea34b8b5beaee8ae0c470eda455323f337543d
belutkautka <92267872+belutkautka@users.noreply.github.com>
2022-05-12 16:54:18 +0500

    all physics is done

--- Game-3.0/Assets/Scripts/TotalNewMovement.cs
+++ Game-3.0/Assets/Scripts/TotalNewMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestMovement : MonoBehaviour
+public class TotalNewMovement : MonoBehaviour
 {
     private PlayerInputSystem input;
 
@@ -17,26 +17,22 @@ public class TestMovement : MonoBehaviour
     private float movementX;
 
     private new Rigidbody2D rigidbody;
+    private GameObject player;
 
     private void Awake()
     {
         input = new PlayerInputSystem();
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        player = gameObject.gameObject;
         
         input.Player.Move.performed += context => Move(context.ReadValue<float>());
         input.Player.Move.canceled += context => Move(0);
         input.Player.Jump.performed += context => Jump();
-        // input.Player.SwapPlayer.performed += context => SwapPlayer();
     }
-
-    /*private static void SwapPlayer()
-    {
-        TotalNewControl.Distribute();
-    }*/
 
     private void Update()
     {
-        // if (ConnectionIsTrue[rigidbody.name])
+        if (TotalNewControl.CheckForConnection(player))
         {
             rigidbody.velocity = new Vector2(movementX, rigidbody.velocity.y);
         }
@@ -55,7 +51,7 @@ public class TestMovement : MonoBehaviour
     
     private void Jump()
     {
-        if (!isGrounded) return;
+        if (!isGrounded || !TotalNewControl.CheckForConnection(player)) return;
         rigidbody.velocity = new Vector2(movementX, jumpForce);
         isGrounded = false;
     }
