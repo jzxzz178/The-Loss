15d5479fb1bf1d6f1656da19385ef7fe394ae989
Fedor <pichuginf2016@mail.ru>
2022-05-13 11:51:00 +0500

    new structure for TabList

--- Game-3.0/Assets/Scripts/TotalNewControl.cs
+++ Game-3.0/Assets/Scripts/TotalNewControl.cs
@@ -15,21 +15,26 @@ public class TotalNewControl : MonoBehaviour
     private static Dictionary<GameObject, bool> linkDictionary = new Dictionary<GameObject, bool>();
     private static int index = -1;
 
+    private static LinkedList<PlayerCell> queueList;
+
+    // private static Queue<GameObject> queue;
+
     private void Awake()
     {
         swapSystem = new SwapSystem();
         swapSystem.SwapPlayer.Swap.performed += context => Distribute();
+        queueList = new LinkedList<PlayerCell>();
+        // queue = new Queue<GameObject>();
     }
 
-    void Start()
+    private void Start()
     {
         players = GameObject.FindGameObjectsWithTag("Player").OrderBy(x => x.name).ToArray();
         foreach (var player in players)
-        {
-            linkDictionary[player] = false;
-        }
+            UpdateDictionary(player, false);
 
-        linkDictionary[players[index + 1]] = true;
+        UpdateDictionary(players[index + 1], true);
+        queueList.AddLast(new PlayerCell(players[index + 1], true));
 
         lines = GameObject.FindGameObjectsWithTag("Line");
         Distribute();
@@ -38,15 +43,16 @@ public class TotalNewControl : MonoBehaviour
     public static void UpdateDictionary(GameObject player, bool value)
     {
         linkDictionary[player] = value;
+        var playerCell = new PlayerCell(player, value);
+        if (value)
+            queueList.AddFirst(playerCell);
+        else
+            queueList.Remove(playerCell);
     }
 
     public static bool CheckForConnection(GameObject player) => linkDictionary[player];
 
-    void Update()
-    {
-    }
-
-    void Distribute()
+    private static void Distribute()
     {
         for (var j = 0; j < players.Length; j++)
         {
@@ -59,8 +65,16 @@ public class TotalNewControl : MonoBehaviour
         var i = 0;
         foreach (var line in lines)
         {
-            if (i == index) i++;
-            line.GetComponent<TotalNewLineMaker>().ChangePlayers(players[index], players[i]);
+            // if (i == index) i++;
+            if (queueList.Last.Value.ActiveFlag)
+            {
+                line.GetComponent<TotalNewLineMaker>()
+                    .ChangePlayers(queueList.Last.Value.Player, queueList.First.Value.Player);
+                queueList.First.Value.ActiveFlag = false;
+                queueList.AddLast(queueList.First.Value);
+                queueList.RemoveFirst();
+            }
+
             i++;
         }
     }
@@ -68,4 +82,29 @@ public class TotalNewControl : MonoBehaviour
     private void OnEnable() => swapSystem.Enable();
 
     private void OnDisable() => swapSystem.Disable();
+
+    public class PlayerCell
+    {
+        public readonly GameObject Player;
+        public PlayerCell Previous;
+        public PlayerCell Next;
+
+
+        public PlayerCell(GameObject player)
+        {
+            Player = player;
+        }
+    }
+
+    public class TabList
+    {
+        public PlayerCell ActivePlayer { get; set; }
+        public PlayerCell Head { get; set; }
+        public PlayerCell Tale { get; set; }
+
+        public void AddFirst(GameObject player)
+        {
+            var newHead = new PlayerCell(player)
+        }
+    }
 }
