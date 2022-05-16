b0f8e2d218c5eea9fd81feec7266ee3fc73a1435
Fedor <pichuginf2016@mail.ru>
2022-05-16 22:29:45 +0500

    created TabList but nothing changed

--- Game-3.0/Assets/Scripts/TotalNewControl.cs
+++ Game-3.0/Assets/Scripts/TotalNewControl.cs
@@ -14,7 +14,7 @@ public class TotalNewControl : MonoBehaviour
     private static GameObject[] players;
     private static GameObject[] lines;
 
-    private static Dictionary<GameObject, bool> linkDictionary = new Dictionary<GameObject, bool>();
+    private static readonly Dictionary<GameObject, bool> LinkDictionary = new Dictionary<GameObject, bool>();
     private static int index = -1;
 
     private void Awake()
@@ -26,58 +26,46 @@ public class TotalNewControl : MonoBehaviour
     private void Start()
     {
         players = GameObject.FindGameObjectsWithTag("Player").OrderBy(x => x.name).ToArray();
+        tabList = new TabList(GameObject.Find("Player"));
+        
         foreach (var player in players)
             UpdateDictionary(player, false);
-
-        UpdateDictionary(players[index + 1], true);
-
-        tabList = new TabList(GameObject.Find("Player"));
-
+        
+        UpdateDictionary(players[0], true);
         lines = GameObject.FindGameObjectsWithTag("Line");
         
         var i = 0;
         foreach (var line in lines)
         {
-            if (players[i] == tabList.ActivePlayer.Player)
+            if (players[i] == tabList.ActivePlayerCell.Player)
                 i++;
             line.GetComponent<TotalNewLineMaker>()
-                .ChangePlayers(tabList.ActivePlayer.Player, players[i]);
+                .ChangePlayers(tabList.ActivePlayerCell.Player, players[i]);
             i++;
         }
     }
 
     public static void UpdateDictionary(GameObject player, bool value)
     {
-        linkDictionary[player] = value;
+        LinkDictionary[player] = value;
+        if (value) tabList.Add(player);
     }
 
-    public static bool CheckForConnection(GameObject player) => linkDictionary[player];
+    public static bool CheckForConnection(GameObject player) => LinkDictionary[player];
 
-    private void Distribute()
+    private static void Distribute()
     {
-        /*for (var j = 0; j < players.Length; j++)
-        {
-            index = (index + 1) % players.Length;
-            if (linkDictionary[players[index]])
-                break;
-            if (j == players.Length - 1) return;
-        }*/
-        // if (!tabList.CanSwap()) return;
+        if (!tabList.IsAbleToSwap()) return;
         var player = tabList.TakePlayerToSwap();
         var i = 0;
         foreach (var line in lines)
         {
             if (players[i] == player)
-                i = (i + 1) % players.Length;
+                i++;
             line.GetComponent<TotalNewLineMaker>()
                 .ChangePlayers(player, players[i]);
             i++;    
             
-            /*if (tabList.CanSwap())
-            {
-                line.GetComponent<TotalNewLineMaker>()
-                    .ChangePlayers(tabList.Tale.Player, tabList.TakePlayerToSwap());
-            }*/
         }
     }
 
@@ -105,27 +93,50 @@ public class TotalNewControl : MonoBehaviour
     /// </summary>
     private class TabList
     {
-        public PlayerCell ActivePlayer;
-        public PlayerCell Head;
-        public PlayerCell Tale;
+        public PlayerCell ActivePlayerCell;
+        private PlayerCell head;
+        private PlayerCell tale;
 
         public TabList(GameObject activePlayer)
         {
-            ActivePlayer = new PlayerCell(activePlayer);
-            Head = Tale = null;
+            ActivePlayerCell = new PlayerCell(activePlayer);
+            head = tale = null;
             foreach (var player in players)
             {
                 if (player != activePlayer)
                     Add(player);
             }
         }
-
-        public bool CanSwap()
+        
+        public void Add(GameObject player)
         {
-            var current = Head;
+            if (ActivePlayerCell != null)
+                if (player == ActivePlayerCell.Player) 
+                    return;
+            
+            if (head == null || tale == null)
+            {
+                tale = head = new PlayerCell(player);
+                return;
+            }
+
+            Remove(player);
+            var newHead = new PlayerCell(player)
+            {
+                Next = head,
+                Previous = null
+            };
+            
+            head.Previous = newHead;
+            head = newHead;
+        }
+
+        public bool IsAbleToSwap()
+        {
+            var current = head;
             while (current != null)
             {
-                if (linkDictionary[current.Player])
+                if (LinkDictionary[current.Player])
                     return true;
                 current = current.Next;
             }
@@ -139,11 +150,11 @@ public class TotalNewControl : MonoBehaviour
         /// <returns></returns>
         public GameObject TakePlayerToSwap()
         {
-            var current = Head;
+            var current = head;
             var player = current.Player;
             while (current != null)
             {
-                if (linkDictionary[current.Player])
+                if (LinkDictionary[current.Player])
                 {
                     player = current.Player;
                     break;
@@ -152,50 +163,55 @@ public class TotalNewControl : MonoBehaviour
                 current = current.Next;
             }
 
-            var newCell = SwapElementWithActivePlayer(current);
-            MoveCellToEnd(newCell);
+            SwapElementWithActivePlayerAndMoveElementToEnd(current);
             return player;
         }
 
-        private PlayerCell SwapElementWithActivePlayer(PlayerCell cell)
+        private void SwapElementWithActivePlayerAndMoveElementToEnd(PlayerCell newActivePlayer)
         {
-            if (cell.Previous != null) cell.Previous.Next = ActivePlayer;
-            if (cell.Next != null) cell.Next.Previous = ActivePlayer;
+            Remove(newActivePlayer.Player);
+            newActivePlayer.Next = null;
+            newActivePlayer.Previous = null;
+            
+            tale.Next = ActivePlayerCell;
+            ActivePlayerCell.Previous = tale;
+            tale = ActivePlayerCell;
 
-            ActivePlayer.Next = cell.Next;
-            ActivePlayer.Previous = cell.Previous;
+            ActivePlayerCell = newActivePlayer;
 
-            var newCell = ActivePlayer;
+            /*if (newActivePlayer.Previous != null) newActivePlayer.Previous.Next = ActivePlayerCell;
+            if (newActivePlayer.Next != null) newActivePlayer.Next.Previous = ActivePlayerCell;
 
-            ActivePlayer = cell;
-            ActivePlayer.Next = null;
-            ActivePlayer.Previous = null;
-            return newCell;
+            ActivePlayerCell.Next = newActivePlayer.Next;
+            ActivePlayerCell.Previous = newActivePlayer.Previous;
+
+            var newCell = ActivePlayerCell;
+
+            ActivePlayerCell = newActivePlayer;
+            ActivePlayerCell.Next = null;
+            ActivePlayerCell.Previous = null;
+            MoveCellToEnd(newCell);*/
         }
 
-        private void Add(GameObject player)
+        private void Remove (GameObject player)
         {
-            if (Head == null || Tale == null)
-            {
-                Tale = Head = new PlayerCell(player);
-                return;
-            }
-
-            var newHead = new PlayerCell(player);
-            Head.Previous = newHead;
-            newHead.Next = Head;
-            Head = newHead;
-        }
-
-        private void Remove (PlayerCell cell)
-        {
-            var current = Head;
+            var current = head;
             while (current != null)
             {
-                if (current == cell)
+                if (current.Player == player)
                 {
                     if (current.Previous != null) current.Previous.Next = current.Next;
                     if (current.Next != null) current.Next.Previous = current.Previous;
+                    if (head == current)
+                    {
+                        head = head.Next;
+                        if (head != null) head.Previous = null;
+                    }
+                    if (tale == current)
+                    {
+                        tale = tale.Previous;
+                        if (tale != null) tale.Next = null;
+                    }
                     break;
                 }
 
@@ -205,12 +221,13 @@ public class TotalNewControl : MonoBehaviour
 
         private void MoveCellToEnd(PlayerCell cell)
         {
-            if (Head == Tale) return;
+            if (head == tale) return;
 
-            Remove(cell);
-            Tale.Next = cell;
-            cell.Previous = Tale;
-            Tale = cell;
+            Remove(cell.Player);
+            tale.Next = cell;
+            cell.Previous = tale;
+            cell.Next = null;
+            tale = cell;
         }
     }
 }
