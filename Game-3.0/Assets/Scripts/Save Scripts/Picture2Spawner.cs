4804f33534ed25e9ca7626b80bb08992e5a79553
Fedor <pichuginf2016@mail.ru>
2022-06-27 14:05:25 +0500

    maybe works

--- Game-3.0/Assets/Scripts/Save Scripts/Picture2Spawner.cs
+++ Game-3.0/Assets/Scripts/Save Scripts/Picture2Spawner.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Picture2Spawner : MonoBehaviour
 {
-    private bool flag;
+    private bool levelAlreadyOpened;
 
     private void Update()
     {
-        if (SceneSaver.CheckForSave("First Scene") && !flag)
+        if (PlayerPrefs.HasKey("1st level") && !levelAlreadyOpened)
         {
             transform.position = new Vector3(-0.14f, 2.94f - 0.88f, 8.8f);
-            flag = true;
+            levelAlreadyOpened = true;
         }
     }
 }
